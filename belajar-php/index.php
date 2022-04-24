2b77441b16ba27c3e5cb2e6a1c5e821ec619293c
Nurfadhilah Itsnaini Djahmiq <101685998+Nurfadhilah11@users.noreply.github.com>
2022-04-25 00:22:06 +0700

    Add files via upload

---
+++ belajar-php/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$nama = "Itsnaini";
+/*
+echo "Halo nama saya ".$nama;
+
+echo "<br>";
+
+// define("NAMA", "Nurfadhilah");
+
+$nama = "Mita";
+
+echo "Halo nama saya ".NAMA;
+*/ 
+
+$nama = array("Nurfadhilah", "Aji", "Elsa");
+
+/*echo "Halo nama saya ".$nama[0]." dia punya teman dengan nama ".$nama[1]. ", kami memiliki mentor yang bernama ".$nama[2]; */
+
+print_r($nama);
+
+?>
